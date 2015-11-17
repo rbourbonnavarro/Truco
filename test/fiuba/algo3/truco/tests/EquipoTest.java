f92d9271982232f11a0af692a65ee2255427922c
fedemgp <fedemgp@gmail.com>
2015-11-17 02:04:06 -0300

    Cambiado equals de Carta

--- test/fiuba/algo3/truco/tests/EquipoTest.java
+++ test/fiuba/algo3/truco/tests/EquipoTest.java
@@ -1,9 +1,11 @@
 package fiuba.algo3.truco.tests;
 
-import fiuba.algo3.truco.modelo.Equipo;
-import fiuba.algo3.truco.modelo.EquipoSinIntegrantesException;
-import fiuba.algo3.truco.modelo.Jugador;
+import fiuba.algo3.truco.modelo.*;
+import fiuba.algo3.truco.modelo.EnvidoYFlor.CalculadorTanto;
 
+import fiuba.algo3.truco.modelo.EnvidoYFlor.JuegoSinFlor;
+import fiuba.algo3.truco.modelo.Palo.Basto;
+import fiuba.algo3.truco.modelo.Palo.Espada;
 import fiuba.algo3.truco.modelo.Puntos.Buenas;
 import fiuba.algo3.truco.modelo.Puntos.Malas;
 import org.junit.Assert;
@@ -12,17 +14,21 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class EquipoTest {
 
     private Equipo equipo;
     private List<Jugador> jugadores;
+    private Jugador jugador1;
+    private Jugador jugador2;
 
     @Before
     public void setUp() {
-
-        this.jugadores = new ArrayList<>(Arrays.asList(new Jugador("J1"), new Jugador("J2")));
+        this.jugador1 = new Jugador("J1");
+        this.jugador2 = new Jugador("J2");
+        this.jugadores = new ArrayList<>(Arrays.asList(jugador1, jugador2));
 
         this.equipo = new Equipo("Equipo 1", this.jugadores);
 
@@ -70,6 +76,24 @@ public class EquipoTest {
         this.equipo.sumarPuntos(20);
         Assert.assertEquals(this.equipo.getPuntaje().getEstadoPuntaje(),new Buenas());
         Assert.assertEquals(this.equipo.getPuntos(),20);
+    }
+    @Test
+    public void test8calcularEnvidoDevuelveElEnvidoMasAltoDelEquipo(){
+        Carta carta1 = new Carta(7,new Espada());
+        Carta carta2 = new Carta(3,new Espada());
+        Carta carta3 = new Carta(2,new Basto());
+
+        Carta carta4 = new Carta(7,new Basto());
+        Carta carta5 = new Carta(6,new Basto());
+        Carta carta6 = new Carta(2,new Basto());
+
+        LinkedList<Carta> cartas1 = new LinkedList<>(Arrays.asList(carta1,carta2,carta3));
+        LinkedList<Carta> cartas2 = new LinkedList<>(Arrays.asList(carta4,carta5,carta6));
+
+        jugador1.setMano(new Mano(cartas1,new JuegoSinFlor()));
+        jugador2.setMano(new Mano(cartas2,new JuegoSinFlor()));
+
+        Assert.assertEquals(33,this.equipo.calcularEnvido());
     }
 
 }
