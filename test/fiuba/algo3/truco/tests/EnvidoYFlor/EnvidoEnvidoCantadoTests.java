6829e8b660eb132cbff97d000ef52bbfce0ceb78
fedemgp <fedemgp@gmail.com>
2015-11-16 23:44:58 -0300

    a ver si reconoce envidoEnvidoCantado Test

--- test/fiuba/algo3/truco/tests/EnvidoYFlor/EnvidoEnvidoCantadoTests.java
+++ test/fiuba/algo3/truco/tests/EnvidoYFlor/EnvidoEnvidoCantadoTests.java
@@ -11,39 +11,39 @@ import org.junit.Test;
 public class EnvidoEnvidoCantadoTests {
 
     EstadoEnvido envido;
-
-    @Before
-    public void setUp(){
-        envido = new EnvidoEnvidoCantado();
-    }
-
     @Test
     public void Test1PuntosDevuelve4(){
+        envido = new EnvidoEnvidoCantado();
         Assert.assertEquals(envido.puntos(), 4);
     }
 
     @Test
     public void Test2NoQueridoDevuelve2Puntos(){
+        envido = new EnvidoEnvidoCantado();
         Assert.assertEquals(envido.noQuerido(),2);
     }
 
     @Test(expected = NoSePuedeCantarEnvido.class)
     public void Test3NoSePuedeCantarEnvido(){
+        envido = new EnvidoEnvidoCantado();
         envido.envido();
     }
 
     @Test(expected = NoSePuedeCantarEnvidoEnvido.class)
     public void Test4SePuedeCantarEnvidoEnvido(){
+        envido = new EnvidoEnvidoCantado();
         envido.envidoEnvido();
     }
 
     @Test
     public void Test5SePuedeCantarRealEnvido(){
+        envido = new EnvidoEnvidoCantado();
         Assert.assertEquals(envido.realEnvido(), new RealEnvidoCantado(3));
     }
 
     @Test
     public void Test6SePuedeCantarFaltaEnvido(){
+        envido = new EnvidoEnvidoCantado();
         Puntaje puntos = new Puntaje();
         puntos.sumar(19);
         Assert.assertEquals(envido.faltaEnvido(puntos),new FaltaEnvidoCantado(puntos));
