20d159a8ae518a5cfc5e4114f44dbdd9bc7133e6
fedemgp <fedemgp@gmail.com>
2015-11-28 05:19:27 -0300

    Vistas, se puede abrir con scene builder

--- src/fiuba/algo3/truco/vista/Main.java
+++ src/fiuba/algo3/truco/vista/Main.java
@@ -1,5 +1,8 @@
 package fiuba.algo3.truco.vista;
 
+import fiuba.algo3.truco.modelo.Equipo;
+import fiuba.algo3.truco.modelo.Jugador;
+import fiuba.algo3.truco.modelo.Mesa;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,11 +11,15 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Main extends Application {
 
     private Stage ventanaPrincipal;
     private BorderPane diseñoRaiz;
+    private Mesa mesa;
+    private Equipo equipo1;
+    private Equipo equipo2;
 
     @Override
     public void start(Stage primaryStage) {
@@ -20,14 +27,33 @@ public class Main extends Application {
         this.ventanaPrincipal.setTitle("Truco");
 
         iniciarVentanaRaiz();
+        this.equipo1 = new Equipo("equipo1",Arrays.asList(new Jugador("J1")));
+        this.equipo2 = new Equipo("equipo1",Arrays.asList(new Jugador("J1")));
+        this.mesa = new Mesa(equipo1, equipo2,false);
+        iniciarPartidaDeADos();
+    }
 
+    private void iniciarPartidaDeADos() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("juegoDeADos.fxml"));
+            AnchorPane partidaDeADos = loader.load();
+
+            diseñoRaiz.setCenter(partidaDeADos);
+
+     //       ControladorVentanaJuego controlador =loader.getController();
+       //     controlador.setMain(this,this.mesa,equipo1,equipo2);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void iniciarVentanaRaiz() {
         try {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(Main.class.getResource("diseñoRaiz.fxml"));
-            this.diseñoRaiz = (BorderPane) loader.load();
+            this.diseñoRaiz = loader.load();
 
             Scene scene = new Scene(this.diseñoRaiz);
             this.ventanaPrincipal.setScene(scene);
