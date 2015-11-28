edb5b7cddbe03f77929d55335a9170e35a1a063f
fedemgp <fedemgp@gmail.com>
2015-11-28 01:41:29 -0300

    Vistas, se puede abrir con scene builder

---
+++ src/fiuba/algo3/truco/ejemploMVC/Controller.java
@@ -0,0 +1,4 @@
+package fiuba.algo3.truco.ejemploMVC;
+
+public class Controller {
+}
---
+++ src/fiuba/algo3/truco/ejemploMVC/modelo/Person.java
@@ -0,0 +1,7 @@
+package fiuba.algo3.truco.ejemploMVC.modelo;
+
+/**
+ * Created by GomezPeter on 28/11/2015.
+ */
+public class Person {
+}
