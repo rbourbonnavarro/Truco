bc5d253b66295c7fbdc334785a9207c980adbd08
Rodrigo <rodrigobourbon44@gmail.com>
2015-12-02 11:17:40 -0300

    Sin el error de las cartass, vaaamooooooo

--- test/fiuba/algo3/truco/tests/MazoTest.java
+++ test/fiuba/algo3/truco/tests/MazoTest.java
@@ -1,13 +1,9 @@
 package fiuba.algo3.truco.tests;
 
-import com.sun.javaws.exceptions.InvalidArgumentException;
 import fiuba.algo3.truco.modelo.Carta;
 import fiuba.algo3.truco.modelo.Jugador;
 import fiuba.algo3.truco.modelo.Mazo;
-import fiuba.algo3.truco.vista.DiccionarioCartas;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -25,8 +21,11 @@ public class MazoTest {
 
         this.jugadores = new ArrayList<>();
 
-        this.jugadores.add(new Jugador("J1"));
-        this.jugadores.add(new Jugador("JIA"));
+        for(int i = 0; i < 13; i++) {
+
+            this.jugadores.add(new Jugador("Jugador " + i));
+
+        }
 
     }
 
@@ -35,29 +34,20 @@ public class MazoTest {
 
         this.mazo.repartir(this.jugadores);
 
-        for(Carta cartaJ1 : this.jugadores.get(0).obtenerCartas()) {
+        for(int i = 0; i < this.jugadores.size() - 1; i++) {
 
-            for(Carta cartaJ2 : this.jugadores.get(1).obtenerCartas()) {
+            for (Carta cartaJ1 : this.jugadores.get(i).obtenerCartas()) {
 
-                Assert.assertNotEquals(cartaJ1, cartaJ2);
+                for (Carta cartaJ2 : this.jugadores.get(i + 1).obtenerCartas()) {
+
+                    Assert.assertNotEquals(cartaJ1, cartaJ2);
+
+                }
 
             }
 
         }
 
-    }
-
-    Button boton1 = new Button();
-    DiccionarioCartas diccionarioCartas = new DiccionarioCartas();
-
-    @Test(expected = InvalidArgumentException.class)
-    public void fasdf() {
-
-        for(Carta carta : this.mazo.getCartas()) {
-
-            boton1.setGraphic(new ImageView(diccionarioCartas.representacionCarta(carta)));
-
-        }
 
     }
 
